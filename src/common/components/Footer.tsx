c075c0f39328da692078d8116f9da5bfdf6d5609
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-05-12 14:26:55 -0400

    feat(auth): implement static responsive login page with form with hero image.

---
+++ src/common/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {  
+  return (
+    <footer className="text-[14px] font-semibold text-slate-400 text-center p-6">
+      <p>Sidekick v1.0.0</p>
+    </footer>
+  );
+};
+
+export default Footer;
